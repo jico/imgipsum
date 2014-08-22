089252f662fff58b1b35154b82ad0f85f7e9ba26
Jico Baligod <jico@baligod.com>
2014-08-22 18:17:37 -0400

    Add default options

--- index.js
+++ index.js
@@ -2,20 +2,29 @@
 
 var Canvas = require('canvas');
 
+var DEFAULTS = {
+  colorBg:     '999',
+  colorFg:     'fff',
+  maxFontSize: 120,
+  maxHeight:   1600,
+  maxWidth:    1600,
+  route:       'imgipsum',
+  textPadding: 0.1
+};
+
 module.exports = function() {
   return function (req, res, next) {
     var pathTokens = req.path.slice(1).split('/');
 
     // Check route
-    if (pathTokens[0] !== 'imgipsum') return next();
+    if (pathTokens[0] !== DEFAULTS.route) return next();
 
-    // TODO: Handle no params
-
+    // TODO: Handle no params in route
     var dimensions = pathTokens[1].split('x');
-    var width      = parseInt(dimensions[0]);
-    var height     = parseInt(dimensions[1] || width);
-    var colorBg    = pathTokens[2] || '999';
-    var colorFg    = pathTokens[3] || 'fff';
+    var width      = Math.min(parseInt(dimensions[0]), DEFAULTS.maxWidth);
+    var height     = Math.min(parseInt(dimensions[1] || width), DEFAULTS.maxHeight);
+    var colorBg    = pathTokens[2] || DEFAULTS.colorBg;
+    var colorFg    = pathTokens[3] || DEFAULTS.colorFg;
     var text       = req.query.text || ('' + width + 'x' + height);
 
     // Create canvas
@@ -28,11 +37,12 @@ module.exports = function() {
 
     // Find optimal text size
     var textFits = false;
-    var fontSize = 120;
+    var fontSize = DEFAULTS.maxFontSize;
+    var textWidthPercent = 1 - DEFAULTS.textPadding * 2;
     while (!textFits) {
       ctx.font = '' + fontSize + 'px Impact';
       var textMetrics = ctx.measureText(text);
-      if (textMetrics.width > (width * 0.8)) {
+      if (textMetrics.width > (width * textWidthPercent)) {
         fontSize = Math.round(fontSize * 0.9);
       } else {
         textFits = true;
@@ -40,9 +50,9 @@ module.exports = function() {
     }
 
     // Set text
-    ctx.textAlign = 'center';
+    ctx.textAlign    = 'center';
     ctx.textBaseline = 'middle';
-    ctx.fillStyle = '#' + colorFg;
+    ctx.fillStyle    = '#' + colorFg;
     ctx.fillText(text, width / 2, height / 2);
 
     // Send
@@ -50,9 +60,9 @@ module.exports = function() {
     var stream = canvas.pngStream();
     stream.pipe(res, function(err) {
       if (err) {
-        res.send(500, err);
+        return res.send(500, err);
       } else {
-        res.end();
+        return res.end();
       }
     });
   };
