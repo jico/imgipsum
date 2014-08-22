152129a222e7ed7d0d012a638057ffeb43aa9acb
Jico Baligod <jico@baligod.com>
2014-08-22 09:41:06 -0400

    Add default text as dimensions and change font to Impact

--- index.js
+++ index.js
@@ -12,25 +12,25 @@ module.exports = function() {
     // TODO: Handle no params
 
     var dimensions = pathTokens[1].split('x');
-    var width = parseInt(dimensions[0]);
-    var height = parseInt(dimensions[1] || width);
-    var colorBg = pathTokens[2] || '999';
-    var colorFg = pathTokens[3] || 'fff';
-    var text = req.query.text;
+    var width      = parseInt(dimensions[0]);
+    var height     = parseInt(dimensions[1] || width);
+    var colorBg    = pathTokens[2] || '999';
+    var colorFg    = pathTokens[3] || 'fff';
+    var text       = req.query.text || ('' + width + 'x' + height);
 
     // Create canvas
     var canvas = new Canvas(width, height);
-    var ctx = canvas.getContext('2d');
+    var ctx    = canvas.getContext('2d');
 
     // Set background
     ctx.fillStyle = '#' + colorBg;
     ctx.fillRect(0, 0, width, height);
 
-    // Set text
+    // Find optimal text size
     var textFits = false;
     var fontSize = 120;
     while (!textFits) {
-      ctx.font = '' + fontSize + 'px Helvetica';
+      ctx.font = '' + fontSize + 'px Impact';
       var textMetrics = ctx.measureText(text);
       if (textMetrics.width > (width * 0.8)) {
         fontSize = Math.round(fontSize * 0.9);
@@ -39,6 +39,7 @@ module.exports = function() {
       }
     }
 
+    // Set text
     ctx.textAlign = 'center';
     ctx.textBaseline = 'middle';
     ctx.fillStyle = '#' + colorFg;
