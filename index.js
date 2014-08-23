ee8b0efd6838927e6313af62c8ddafe351ff9ab9
Jico Baligod <jico@baligod.com>
2014-08-23 15:31:25 -0400

    Allow passing in options to override defaults

--- index.js
+++ index.js
@@ -12,19 +12,28 @@ var DEFAULTS = {
   textPadding: 0.1
 };
 
-module.exports = function() {
+function merge(obj1, obj2){
+  var obj3 = {};
+  for (var prop1 in obj1) { obj3[prop1] = obj1[prop1]; }
+  for (var prop2 in obj2) { obj3[prop2] = obj3[prop2]; }
+  return obj3;
+}
+
+module.exports = function(options) {
+  options = merge(DEFAULTS, options);
+
   return function (req, res, next) {
     var pathTokens = req.path.slice(1).split('/');
 
     // Check route
-    if (pathTokens[0] !== DEFAULTS.route) return next();
+    if (pathTokens[0] !== options.route) return next();
 
     // TODO: Handle no params in route
     var dimensions = pathTokens[1].split('x');
-    var width      = Math.min(parseInt(dimensions[0]), DEFAULTS.maxWidth);
-    var height     = Math.min(parseInt(dimensions[1] || width), DEFAULTS.maxHeight);
-    var colorBg    = pathTokens[2] || DEFAULTS.colorBg;
-    var colorFg    = pathTokens[3] || DEFAULTS.colorFg;
+    var width      = Math.min(parseInt(dimensions[0]), options.maxWidth);
+    var height     = Math.min(parseInt(dimensions[1] || width), options.maxHeight);
+    var colorBg    = pathTokens[2] || options.colorBg;
+    var colorFg    = pathTokens[3] || options.colorFg;
     var text       = req.query.text || ('' + width + 'x' + height);
 
     // Create canvas
@@ -37,8 +46,8 @@ module.exports = function() {
 
     // Find optimal text size
     var textFits = false;
-    var fontSize = DEFAULTS.maxFontSize;
-    var textWidthPercent = 1 - DEFAULTS.textPadding * 2;
+    var fontSize = options.maxFontSize;
+    var textWidthPercent = 1 - options.textPadding * 2;
     while (!textFits) {
       ctx.font = '' + fontSize + 'px Impact';
       var textMetrics = ctx.measureText(text);
